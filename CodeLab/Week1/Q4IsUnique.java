d879d95008f7399f76ddbb1bdeb3373e4684dae9
Abbott <71251245+abbottqwq@users.noreply.github.com>
2020-09-15 00:15:37 -0400

    upload week1 codes

---
+++ CodeLab/Week1/Q4IsUnique.java
@@ -0,0 +1,15 @@
+package Week1;
+
+import java.util.HashSet;
+
+public class Q4IsUnique {
+    public boolean isUnique(String s) {
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++)
+            if (set.contains(s.charAt(i))) return false;
+            else set.add(s.charAt(i));
+        return true;
+    }
+
+
+}
---
+++ CodeLab/Week1/Q5CheckPermutation.java
@@ -0,0 +1,26 @@
+package Week1;
+
+import java.util.HashMap;
+
+public class Q5CheckPermutation {
+    public boolean isPermutation(String s1, String s2) {
+        int l = s1.length();
+        if (l != s2.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < l; i++) {
+            char c = s1.charAt(i);
+            if (map.containsKey(c))
+                map.replace(c, map.get(c) + 1);
+            else map.put(c, 1);
+            c = s2.charAt(i);
+            if (map.containsKey(c))
+                map.replace(c, map.get(c) - 1);
+            else map.put(c, -1);
+        }
+        for (int x : map.values())
+            if (x != 0) return false;
+        return true;
+    }
+
+
+}
