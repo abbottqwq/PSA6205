e1a79121aaf4c995b076753725341512c7bcb08d
Abbott <71251245+abbottqwq@users.noreply.github.com>
2020-09-20 16:44:14 -0400

    auto sync off

--- CodeLab/Week2/test2.java
+++ CodeLab/Week2/test2.java
@@ -2,6 +2,6 @@ package CodeLab.Week2;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("hellow world");
+        System.out.println("hellow world;");
     }
 }
