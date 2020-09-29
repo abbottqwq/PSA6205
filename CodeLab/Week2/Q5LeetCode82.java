f58ae7e16126758c39e5eed992165d0ef1443335
Abbott <71251245+abbottqwq@users.noreply.github.com>
2020-09-29 00:46:32 -0400

    reuploadq5

--- CodeLab/Week2/Q5LeetCode82.java
+++ CodeLab/Week2/Q5LeetCode82.java
@@ -1,21 +1,30 @@
 package CodeLab.Week2;
 
-
 public class Q5LeetCode82 {
     class Solution {
         public ListNode deleteDuplicates(ListNode head) {
             if (head == null || head.next == null)
                 return head;
-            ListNode prev = head;
-            ListNode p = head.next;
-            while (p != null) {
-                while (p.val == prev.val && p != null)
+            ListNode hiddenhead = new ListNode(head.val - 1, head);
+            ListNode prev = hiddenhead;
+            ListNode p = hiddenhead.next;
+            while (p.next != null) {
+                if (p.next.val == p.val) {
+                    while (p.next.val == p.val) {
+                        p = p.next;
+                        if (p.next == null) {
+                            prev.next = null;
+                            return hiddenhead.next;
+                        }
+                    }
+                    prev.next = p.next;
+                    p = prev.next;
+                } else {
+                    prev = p;
                     p = p.next;
-                prev.next = p;
-                p = p.next;
-                prev = prev.next;
+                }
             }
-            return head;
+            return hiddenhead.next;
         }
     }
 }
---
+++ CodeLab/Week2/Q6LeetCode25.java
@@ -0,0 +1,22 @@
+package CodeLab.Week2;
+
+public class Q6LeetCode25 {
+    class solution {
+        public ListNode reverse1Group(ListNode firstNode, int k) {
+            if(k == 0)return firstNode;
+            return reverse1Group(firstNode, k - 1);
+        }
+
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode p = head;
+            while (p != null) {
+                int count = 0;
+                while(p!=null && count < k){
+
+                }
+            }
+            return head;
+        }
+
+    }
+}
